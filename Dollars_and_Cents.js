4c5fe8415f04564c641ed92777c47ed67c9aefa9
kamel-b <46602722+kamel-b@users.noreply.github.com>
2019-06-20 12:05:16 +0200

    Delete Dollars_and_Cents.js

--- Dollars_and_Cents.js
+++
@@ -1,9 +0,0 @@
-// // The rest of your team will make sure that the argument is sanitized before being passed to your function although 
-// you will need to account for adding trailing zeros if they are missing (though you won't 
-// have to worry about a dangling period).
-
-const formatMoney =(amount) => {
-    return '$' + amount.toFixed(2)
-  }
-  
-
